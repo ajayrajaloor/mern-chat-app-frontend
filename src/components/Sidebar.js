6351a0fe59370e315990c75a6b2b96014e3f9b44
ajayrajaloor <ajayrajaloor@gmail.com>
2024-11-21 11:42:15 +0530

    + fetch user details and show it in the search user + added loading while search user +searchcard for the user added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,19 +6,24 @@ import { BiLogOut } from "react-icons/bi"
 import Avatar from './Avatar'
 import {useSelector} from 'react-redux'
 import EditUserDetails from './EditUserDetails'
+import Divider from './Divider'
+import {FiArrowUpLeft} from 'react-icons/fi'
+import SearchUser from './SearchUser'
 
 const Sidebar = () => {
   const user = useSelector((state) => state.user || {})
   const [editUserOpen,setEditUserOpen] = useState(false)
+  const [alluser,setAllUser] = useState([])
+  const [openSearchUser,setOpenSearchUser] = useState(false)
 
   return (
-    <div className='h-full w-full'>
+    <div className='h-full w-full grid grid-cols-[48px,1fr] bg-white'>
       <div className='bg-slate-100 w-12 h-full rounded-tr-lg rounded-br-lg py-5 text-slate-600 flex flex-col justify-between'>
         <div>
           <NavLink className={({ isActive }) => `h-12 w-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded ${isActive && "bg-slate-200"}`} title='chat'>
             <IoChatbubbleEllipses size={20} />
           </NavLink>
-          <div title='add friend' className='h-12 w-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded'>
+          <div title='add friend' onClick={()=> setOpenSearchUser(true)} className='h-12 w-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded'>
             <FaUserPlus size={20} />
           </div>
         </div>
@@ -39,10 +44,41 @@ const Sidebar = () => {
         </div>
       </div>
 
+      <div className='w-full'>
+    <div className='h-16 flex items-center'>
+    <h2 className='text-xl font-bold p-4 text-slate-800'>Message</h2>
+    </div>
+    <div className='bg-slate-200 p-[0.5px]'></div>
+
+    <div className=' h-[calc(100vh-65px)] overflow-x-hidden overflow-y-auto scrollbar'>
+      {
+        alluser.length === 0 && (
+          <div className='mt-12'>
+            <div className='flex justify-center items-center my-4 text-slate-500'>
+              <FiArrowUpLeft
+              size={50}
+              />
+            </div>
+            <p className='text-lg text-center text-slate-400'>Explore users to start a conversation with.</p>
+          </div>
+        )
+      }
+    </div>
+      </div>
+
       {/**edit user details */}
       {editUserOpen && (
         <EditUserDetails onClose={()=> setEditUserOpen(false)} user={user}/>
       )}
+
+
+      {/**search user */}
+      {
+        openSearchUser && (
+          <SearchUser onclose={() => setOpenSearchUser(false)} />
+        )
+      }
+
     </div>
   )
 }
---
+++ src/components/UserSearchCard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Avatar from './Avatar'
+
+const UserSearchCard = ({user}) => {
+    return (
+        <div className='flex items-center gap-3 p-2 lg:p-4 border border-transparent border-b-slate-200 hover:border hover:border-primary rounded cursor-pointer'>
+            <div>
+                <Avatar
+                    width={50}
+                    height={50}
+                    name={user?.name}
+                />
+            </div>
+            <div>
+                <div className='font-semibold text-ellipsis line-clamp-1'>
+                    {user?.name}
+                </div>
+                <p className='text-sm text-ellipsis line-clamp-1'>{user?.email}</p>
+            </div>
+        </div>
+    )
+}
+
+export default UserSearchCard
