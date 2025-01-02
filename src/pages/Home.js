4b91b8d63609f0960218679638e4c0c5b98eba64
ajayrajaloor <ajayrajaloor@gmail.com>
2025-01-02 20:46:15 +0530

    added socket url

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -43,7 +43,8 @@ const Home = () => {
   /**socket connection */
   
   useEffect(()=>{
-    const socketConnection = io(process.env.REACT_APP_BACKEND_URL,{
+    // const socketConnection = io(process.env.REACT_APP_BACKEND_URL,{
+    const socketConnection = io(`wss://mern-chat-app-backend-6ikw.onrender.com/api/user-details`,{
       auth :{
         token : localStorage.getItem('token')
       },
